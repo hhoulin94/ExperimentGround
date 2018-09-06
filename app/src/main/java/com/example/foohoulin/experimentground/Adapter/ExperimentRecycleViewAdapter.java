8d3f8c3f5cd2c710c65994b46d8f7f398b6d413e
Foo Hou Lin <hl.foo@lelong.com.my>
2018-09-06 17:22:06 +0800

    trying to do account authentication constraint layout attempt

--- app/src/main/java/com/example/foohoulin/experimentground/Adapter/ExperimentRecycleViewAdapter.java
+++ app/src/main/java/com/example/foohoulin/experimentground/Adapter/ExperimentRecycleViewAdapter.java
@@ -1,11 +1,13 @@
 package com.example.foohoulin.experimentground.Adapter;
 import com.example.foohoulin.experimentground.R;
+import com.squareup.picasso.Picasso;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -30,14 +32,20 @@ public class ExperimentRecycleViewAdapter extends RecyclerView.Adapter<RecyclerV
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, final int position) {
         ExperimentViewHolder holder = (ExperimentViewHolder) viewHolder;
         holder.experiment_title.setText(String.valueOf(item.get(position)));
+        holder.experiment_content.setText(String.valueOf(item.get(position)));
+        Picasso.get().load("https://c.76.my/UserImages/Items/TB220/197/859/197859398.jpg").into(holder.experiment_image);
     }
 
     public static class ExperimentViewHolder extends RecyclerView.ViewHolder {
         TextView experiment_title ;
+        TextView experiment_content ;
+        ImageView experiment_image ;
 
         public ExperimentViewHolder(View v) {
             super(v);
             experiment_title = v.findViewById(R.id.experiment_title) ;
+            experiment_content = v.findViewById(R.id.experiment_content);
+            experiment_image = v.findViewById(R.id.experiment_image);
         }
     }
 
--- app/src/main/java/com/example/foohoulin/experimentground/MainActivity.java
+++ app/src/main/java/com/example/foohoulin/experimentground/MainActivity.java
@@ -2,14 +2,11 @@ package com.example.foohoulin.experimentground;
 
 import android.graphics.Color;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.foohoulin.experimentground.Adapter.ExperimentRecycleViewAdapter;
 
@@ -17,8 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    FloatingActionButton fab ;
 
     RecyclerView experimentRecycleView ;
     ExperimentRecycleViewAdapter experimentRecycleViewAdapter ;
@@ -32,13 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        fab = findViewById(R.id.fab);
         experimentRecycleView = findViewById(R.id.experiment_recycler_view);
 
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
-
-        setTitle("HAHAHAHA");
+        setTitle("Title");
 
         experimentRecycleView.setHasFixedSize(true);
         experimentRecycleView.setLayoutManager(new GridLayoutManager(this , 2));
@@ -53,13 +46,6 @@ public class MainActivity extends AppCompatActivity {
         experimentRecycleView.setAdapter(experimentRecycleViewAdapter);
 
         appbar = findViewById(R.id.appbar);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v , "Hello World" , Snackbar.LENGTH_SHORT).show();
-            }
-        });
 
     }
 }
